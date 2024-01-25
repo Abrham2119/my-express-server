9c5070d4362d33ae56c35818ae9788482d0ed394
Abrham2119 <abrhamzewdu2119.com>
2024-01-25 13:43:06 +0300

    added the response

--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+
+})
+
 app.listen(3001, function() {
     console.log('Server is running on port 3001');
 });
