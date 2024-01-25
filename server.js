9d15533fb702c1be52ab8c10042c8a2475f20ca2
Abrham2119 <abrhamzewdu2119.com>
2024-01-25 07:44:52 +0300

    intalize the server js file

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 //jshint esversions
 
-const express = require('express');
+var express = require('express')
+var app = express()
+
+app.listen(3001, function() {
+    console.log('Server is running on port 3001');
+});
+
+
